8452d1d1a03e6c15cbea04ea9042326a603d7f6e
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-08-08 14:47:14 +0300

    creates update the product in productcontroller

--- Backend/Controllers/ProductController.cs
+++ Backend/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using Backend.Extension;
 using Backend.Model;
 using Backend.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers
 {
@@ -97,5 +98,64 @@ namespace Backend.Controllers
             }
             return _response;
         }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<ApiResponse>> UpProduct(int id, [FromForm]ProductUpdateDTO productUpdateDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (productUpdateDTO == null || id !=productUpdateDTO.Id)
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        return BadRequest();
+                    }
+
+                    // for update
+                    Product productToUpdate = await _context.Products.FirstOrDefaultAsync(u => u.Id == id);
+                    // Product productToUpdate = await _context.Products.FindAsync(id);
+                    if (productToUpdate == null)
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        return BadRequest();
+                    }
+                    //
+
+
+                    productToUpdate.Title = productUpdateDTO.Title;
+                    productToUpdate.Price = productUpdateDTO.Price;
+                    productToUpdate.Category = productUpdateDTO.Category;
+                    productToUpdate.Description = productUpdateDTO.Description;
+                    productToUpdate.ProductType = productUpdateDTO.ProductType;
+                    
+                    // for update image
+                    if(productToUpdate.Image!=null)
+                    {
+                        productToUpdate.Image = await _fileStorage.UpdateFile(containerName, productUpdateDTO.File,productToUpdate.Image);
+                    }
+                   
+                    _context.Products.Update(productToUpdate);
+                    await _context.SaveChangesAsync();
+                    _response.Result = productToUpdate;
+                    _response.StatusCode=HttpStatusCode.NoContent;
+                    return  Ok(_response);
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        
     }
 }
