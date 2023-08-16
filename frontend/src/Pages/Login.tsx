9691513d31421e46a0277e4a6f49780c67ddd074
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-08-16 12:21:16 +0300

    updated login to accomodate sign in data

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { inputHelper } from '../Components/Helper';
+import { useLoginUserMutation } from '../Apis/userApi';
+import { Loader } from '../Components/common';
+import { useDispatch } from 'react-redux';
+import { apiResponse } from '../types';
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const [loginUser] = useLoginUserMutation();
+  const [loading, setLoading] = useState(false);
+  const [userInput, setUserInput] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const handleUserInput = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const receiveData = inputHelper(event, userInput);
+    setUserInput(receiveData);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+    const response: apiResponse = await loginUser({
+      userName: userInput.userName,
+      password: userInput.password,
+    });
+    if (response.data) {
+      console.log(response.data);
+      const { token } = response.data.result;
+      localStorage.setItem("token", token);
+    } else if (response.error) {
+      console.log(response.error.data.errorMessages[0], "error");
+       setError(response.error.data.errorMessages[0]);
+    }
+    setLoading(false);
+  };
     return (
       <div className="container text-center">
-        {/* {loading && <MainLoader />} */}
-        <form method="post">
+        {loading && <Loader />}
+        <form method="post" onSubmit={handleSubmit}>
           <h1 className="mt-5">Login</h1>
           <div className="mt-5">
             <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -14,8 +52,8 @@ const Login = () => {
                 placeholder="Enter Username"
                 required
                 name="userName"
-                // value={userInput.userName}
-                // onChange={handleUserInput}
+                value={userInput.userName}
+                onChange={handleUserInput}
               />
             </div>
 
@@ -26,14 +64,14 @@ const Login = () => {
                 placeholder="Enter Password"
                 required
                 name="password"
-                // value={userInput.password}
-                // onChange={handleUserInput}
+                value={userInput.password}
+                onChange={handleUserInput}
               />
             </div>
           </div>
 
           <div className="mt-2">
-            {/* {error && <p className="text-danger">{error}</p>} */}
+            {error && <p className="text-danger">{error}</p>}
             <button
               type="submit"
               className="btn btn-success"
