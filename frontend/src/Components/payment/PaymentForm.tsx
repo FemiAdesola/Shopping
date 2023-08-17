ef731450990b1f7a48a75d8ae1aec3f42249a742
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-08-17 13:41:51 +0300

    Updated the paymentform for stripe payment

--- frontend/src/Components/payment/PaymentForm.tsx
+++ frontend/src/Components/payment/PaymentForm.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import { PaymentElement } from "@stripe/react-stripe-js";
+import React, { useState } from 'react';
+import {
+  useStripe,
+  useElements,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
+import { toastNotification } from '../Helper';
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleSubmit = async (event : React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+    setIsProcessing(true);
+
+    const result = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: "https://example.com/order/123/complete",
+      },
+     redirect: "if_required",
+    });
+
+    if (result.error) {
+      toastNotification("An unexpected error occured.", "error");
+      setIsProcessing(false);
+    } else {
+      console.log(result);
+    }
+  };
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <PaymentElement />
         <button className="btn btn-success mt-5 w-100">Submit</button>
       </form>
