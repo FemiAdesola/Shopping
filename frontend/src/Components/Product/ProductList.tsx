eb42addfb12b378b049f18028e9d619610469f40
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-08-23 15:20:12 +0300

    implement search functionality

--- frontend/src/Components/Product/ProductList.tsx
+++ frontend/src/Components/Product/ProductList.tsx
@@ -1,60 +1,114 @@
-import React from 'react';
+import React from "react";
 import { useState, useEffect } from "react";
-import { ProductType } from '../../types';
-import  ProductCard  from './ProductCard';
-import { useGetProductsQuery } from '../../Apis/productApi';
-import { useDispatch, useSelector } from 'react-redux';
-import { setProduct } from '../../Redux/productSlice';
-import { MainLoader } from '../common';
-import { RootState } from '../../Redux/store';
+import { ProductType } from "../../types";
+import ProductCard from "./ProductCard";
+import { useGetProductsQuery } from "../../Apis/productApi";
+import { useDispatch, useSelector } from "react-redux";
+import { setProduct } from "../../Redux/productSlice";
+import { MainLoader } from "../common";
+import { RootState } from "../../Redux/store";
 
 const ProductList = () => {
   const dispatch = useDispatch();
   const { data, isLoading } = useGetProductsQuery(null);
   const [products, setProducts] = useState<ProductType[]>([]);
 
-    const searchValue= useSelector(
-      (state: RootState) => state.searchStore.search
-    );
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [categoryList, setCategoryList] = useState([""]);
 
-    useEffect(() => {
-      if (data && data.result) {
-        // dispatch(setProduct(data.result));
-        const searchProductArray = handleFilters(searchValue);
-        setProducts(searchProductArray);
+  const searchValue = useSelector(
+    (state: RootState) => state.searchStore.search
+  );
+
+  const handleCategoryClick = (i: number) => {
+    const buttons = document.querySelectorAll(".custom-buttons");
+    let localCategory;
+    buttons.forEach((button, index) => {
+      if (index === i) {
+        button.classList.add("active");
+        if (index === 0) {
+          localCategory = "All";
+        } else {
+          localCategory = categoryList[index];
+        }
+        setSelectedCategory(localCategory);
+        const dataArray = handleFilters(localCategory, searchValue);
+        setProducts(dataArray);
+      } else {
+        button.classList.remove("active");
       }
-    }, [searchValue]);
-  
+    });
+  };
+
+  useEffect(() => {
+    if (data && data.result) {
+      // dispatch(setProduct(data.result));
+      const searchProductArray = handleFilters(selectedCategory, searchValue);
+      setProducts(searchProductArray);
+    }
+  }, [searchValue]);
+
   useEffect(() => {
     if (!isLoading) {
       dispatch(setProduct(data.result));
       setProducts(data.result);
+
+      const categoryListData = ["All"];
+      data.result.forEach((item: ProductType) => {
+        if (categoryListData.indexOf(item.category) === -1) {
+          categoryListData.push(item.category);
+        }
+      });
+      setCategoryList(categoryListData);
     }
   }, [isLoading]);
 
-
   const handleFilters = (
-    search: string
+    search: string,
+    category: string
   ) => {
     let searchArray = [...data.result];
     //search functionality
     if (search) {
       const newSearchArray = [...searchArray];
       searchArray = newSearchArray.filter((item: ProductType) =>
-        item.category.toUpperCase().includes(search.toUpperCase())
+        item.title.toUpperCase().includes(search.toUpperCase())
       );
     }
     return searchArray;
-  }
-
-
+  };
 
   if (isLoading) {
-    return <div> <MainLoader/></div>;
+    return (
+      <div>
+        {" "}
+        <MainLoader />
+      </div>
+    );
   }
-  
+
   return (
     <div className="container row">
+      <div className="my-3">
+        <ul className="nav w-100 d-flex justify-content-center">
+          {categoryList.map((categoryName, index) => (
+            <li
+              className="nav-item"
+              style={{ ...(index === 0 && { marginLeft: "auto" }) }}
+              key={index}
+            >
+              <button
+                className={`nav-link p-0 pb-2 custom-buttons fs-5 ${
+                  index === 0 && "active"
+                } `}
+                onClick={() => handleCategoryClick(index)}
+              >
+                {categoryName}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
       {products.length > 0 &&
         products.map((product: ProductType, index: number) => (
           <ProductCard product={product} key={index} />
